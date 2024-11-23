e28f1f5704505617cdf8c65d40da36c3a49e99f8
poornapunith <poornachandra813@gmail.com>
2024-11-23 15:29:18 +0530

    FastCommerce Application started

---
+++ src/main/java/com/poorna/fastcommerce/FastCommerceApplication.java
@@ -0,0 +1,14 @@
+package com.poorna.fastcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCommerceApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/poorna/fastcommerce/FastCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poorna.fastcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
