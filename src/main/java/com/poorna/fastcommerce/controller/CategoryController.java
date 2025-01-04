cc772a7e9482aa24138b09304b04e5606f435460
poornapunith <poornachandra813@gmail.com>
2025-01-04 13:31:09 +0530

    Added Splunk integration *Not Working

--- src/main/java/com/poorna/fastcommerce/controller/CategoryController.java
+++ src/main/java/com/poorna/fastcommerce/controller/CategoryController.java
@@ -3,6 +3,10 @@ package com.poorna.fastcommerce.controller;
 import com.poorna.fastcommerce.model.Category;
 import com.poorna.fastcommerce.service.CategoryService;
 import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -11,9 +15,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api")
 public class CategoryController {
+
+    Logger log = (Logger) LogManager.getLogger(CategoryController.class);
 
     @Autowired
     CategoryService categoryService;
@@ -21,24 +28,28 @@ public class CategoryController {
     @GetMapping("/public/categories")
     public ResponseEntity<List<Category>> getAllCategories()
     {
+        log.info("getAllCategories");
         return categoryService.getAllCategories();
     }
 
     @PostMapping("/public/categories")
     public ResponseEntity<String> createCategory(@Valid @RequestBody Category category)
     {
+        log.info("createCategory"+ category);
         return categoryService.createCategory(category);
     }
 
     @DeleteMapping("/admin/categories/{id}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long id)
     {
+        log.info("deleteCategory"+ id);
         return categoryService.deleteCategory(id);
     }
 
     @PutMapping("/public/categories/{id}")
     public ResponseEntity<String> updateCategory(@PathVariable Long id, @RequestBody Category category)
     {
+        log.info("updateCategory"+ id);
         return categoryService.updateCategory(id,category);
     }
 
